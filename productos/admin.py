3421f0f3c933a859aa52c9adae53ff5a394fdadd
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-29 16:40:32 -0300

    creo admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Producto, Cliente, Pedido
+
+
+@admin.register(Producto)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'cantidad')
+    search_fields = ('nombre',)
+
+
+@admin.register(Cliente)
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo')
+
+
+@admin.register(Pedido)
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'cliente', 'fecha')
+    list_filter = ('fecha',)
