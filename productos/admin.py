821ec80b9ae0e3864dcf7cf1df72f15c746b202f
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-29 16:56:26 -0300

    creo admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,7 +1,7 @@
+# productos/admin.py
 from django.contrib import admin
 
-from .models import Producto, Cliente, Pedido
-
+from .models import Producto, Cliente, PerfilCliente, Pedido
 
 @admin.register(Producto)
 
@@ -10,13 +10,17 @@ class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'cantidad')
     search_fields = ('nombre',)
 
-
 @admin.register(Cliente)
 
 
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'correo')
 
+@admin.register(PerfilCliente)
+
+
+class PerfilClienteAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'telefono')
 
 @admin.register(Pedido)
 
@@ -24,3 +28,4 @@ class ClienteAdmin(admin.ModelAdmin):
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('numero', 'cliente', 'fecha')
     list_filter = ('fecha',)
+    filter_horizontal = ('productos',)
