490d6d27c24b0f2006e40cb90c0e3567d3d00584
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-29 16:46:49 -0300

    migraciones

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,4 @@
+# productos/models.py
 from django.db import models
 
 # Modelo sin relaciones
@@ -7,11 +8,12 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField()
+    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
-# Modelo Cliente (para relaciones)
+# Modelo con relaciones
 
 
 class Cliente(models.Model):
@@ -22,21 +24,19 @@ class Cliente(models.Model):
         return self.nombre
 
 
-class PerfilCliente(models.Model):  # Ejemplo OneToOne (1–1)
+
+class PerfilCliente(models.Model):  # uno a uno
     cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=20, blank=True)
 
+    def __str__(self):
+        return f"Perfil de {self.cliente.nombre}"
+
+
 
 class Pedido(models.Model):
-    cliente = models.ForeignKey(  # 1 cliente → muchos pedidos (1–N)
-        Cliente,
-        on_delete=models.CASCADE,
-        related_name='pedidos'
-    )
-    productos = models.ManyToManyField(  # N–N
-        Producto,
-        related_name='pedidos'
-    )
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='pedidos')  # 1 a muchos
+    productos = models.ManyToManyField(Producto, related_name='pedidos')  # muchos a muchos
     fecha = models.DateField(auto_now_add=True)
     numero = models.CharField(max_length=20, unique=True)
 
