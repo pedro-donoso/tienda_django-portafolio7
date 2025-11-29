b63caf8b4731f6e97d046dffafeb192b500da355
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-29 16:48:05 -0300

    edito views productos

--- productos/views.py
+++ productos/views.py
@@ -1,13 +1,51 @@
-# views.py
-from django.views.generic import ListView, UpdateView,DeleteView
-
+# productos/views.py
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView
+)
 from django.urls import reverse_lazy
 
-from django.utils import timezone
-
-from .models import Pedido, Cliente, Producto
-
 from django.db.models import Sum, Count
+
+from .models import Producto, Pedido
+
+
+# CRUD de Producto
+
+
+class ProductoListView(ListView):
+    model = Producto
+    template_name = 'productos/producto_list.html'  # lista
+
+
+class ProductoCreateView(CreateView):
+    model = Producto
+    fields = ['nombre', 'precio', 'cantidad', 'descripcion']
+    template_name = 'productos/producto_form.html'  # crear/editar
+    success_url = reverse_lazy('producto_list')
+
+
+class ProductoUpdateView(UpdateView):
+    model = Producto
+    fields = ['nombre', 'precio', 'cantidad', 'descripcion']
+    template_name = 'productos/producto_form.html'  # crear/editar
+    success_url = reverse_lazy('producto_list')
+
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = 'productos/producto_confirm_delete.html'
+    success_url = reverse_lazy('producto_list')
+
+
+# Listado de pedidos (muestra relaciones)
+
+
+class PedidoListView(ListView):
+    model = Pedido
+    template_name = 'pedidos/pedido_list.html'
+
+
+# Pedidos por cliente y rango de fechas (filter, exclude, annotate)
 
 
 class PedidosPorClienteView(ListView):
@@ -24,42 +62,12 @@ class PedidosPorClienteView(ListView):
         if fecha_inicio and fecha_fin:
             qs = qs.filter(fecha__range=[fecha_inicio, fecha_fin])
 
-        return qs
-
-
-class PedidosResumenView(ListView):
-    model = Pedido
-    template_name = 'pedidos/resumen.html'
-
-    def get_queryset(self):
-        return (
-            Pedido.objects
-            .exclude(productos__cantidad=0)          # exclude()
-            .annotate(total_productos=Count('productos'),
-                      total_items=Sum('productos__cantidad'))  # annotate()
+        # ejemplo de exclude y annotate
+        qs = (
+            qs.exclude(productos__cantidad=0)
+              .annotate(
+                  total_productos=Count('productos'),
+                  total_items=Sum('productos__cantidad')
+              )
         )
-
-
-class ProductoListView(ListView):
-    model = Producto
-    template_name = 'productos/producto_list.html'
-
-
-class ProductoCreateView(CreateView):
-    model = Producto
-    fields = ['nombre', 'precio', 'cantidad', 'descripcion']
-    template_name = 'productos/producto_form.html'
-    success_url = reverse_lazy('producto_list')
-
-
-class ProductoUpdateView(UpdateView):
-    model = Producto
-    fields = ['nombre', 'precio', 'cantidad', 'descripcion']
-    template_name = 'productos/producto_form.html'
-    success_url = reverse_lazy('producto_list')
-
-
-class ProductoDeleteView(DeleteView):
-    model = Producto
-    template_name = 'productos/producto_confirm_delete.html'
-    success_url = reverse_lazy('producto_list')
+        return qs
