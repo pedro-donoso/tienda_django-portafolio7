88f578cde66acdd81a52d2cf9ab12ff128e14fd6
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-11 17:03:21 -0300

    database

--- gestion_tienda/settings.py
+++ gestion_tienda/settings.py
@@ -87,12 +87,6 @@ WSGI_APPLICATION = 'gestion_tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
