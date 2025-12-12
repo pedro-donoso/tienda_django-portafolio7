6988d60415baa5aebc77c749b961621b969d51b7
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-12 09:51:41 -0300

    ajusto settings

--- gestion_tienda/settings.py
+++ gestion_tienda/settings.py
@@ -11,24 +11,11 @@ DATABASES = {
     }
 }
 
-
-
-
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-km0ct8d%key0tqy4o&_zq)-zw7d(-05e_(opx=px+w1nb)wul3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,7 +40,6 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 ROOT_URLCONF = 'gestion_tienda.urls'
 
 TEMPLATES = [
@@ -73,14 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestion_tienda.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -96,10 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -108,14 +82,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-# Archivos estáticos (CSS, JS, imágenes)
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Archivos subidos por el usuario (si los usas)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
