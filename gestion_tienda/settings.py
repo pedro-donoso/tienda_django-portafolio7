69b822898687668347daa96f2763e5b0260cf833
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-12 10:23:35 -0300

    db

--- gestion_tienda/settings.py
+++ gestion_tienda/settings.py
@@ -1,9 +1,12 @@
 import os
 
+import environ
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
